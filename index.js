70ba05380ca59a5c98d6c714fa7020e5f987a165
roelsiemens <roelsiemens@gmail.com>
2025-05-23 14:31:27 +0200

    changes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
         window.addEventListener('scroll', () => {
             const scrolled = window.scrollY;
-              if (window.scrollY > 800) {
-                text.classList.add('Text-white');
-                page.classList.remove('bg-white');
-                page.classList.add('bg-[#171738]');
+                if (window.scrollY > 800) {
+                    text.classList.add('text-white');
+                    text.classList.remove('text-black')
+                    navbar.classList.remove('bg-[#7180b9]');
+                    navbar.classList.add('bg-[#3423a6]');
+                    page.classList.remove('bg-white');
+                    page.classList.add('bg-[#171738]');
                 }         
                 else {
-                page.classList.remove('bg-[#171738]');
-                page.classList.add('bg-white');
+                    text.classList.add('text-black');
+                    text.classList.remove('text-white');
+                    navbar.classList.add('bg-[#7180b9]');
+                    navbar.classList.remove('bg-[#3423a6]');
+                    page.classList.remove('bg-[#171738]');
+                    page.classList.add('bg-white');
                 }  
         })
