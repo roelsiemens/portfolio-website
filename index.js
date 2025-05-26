d5ad5bf6f164dfbd52b4893c7c953939607c8770
roelsiemens <roelsiemens@gmail.com>
2025-05-26 13:14:51 +0200

    changes

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-        window.addEventListener('scroll', () => {
-            const scrolled = window.scrollY;
-                if (window.scrollY > 800) {
-                    text.classList.add('text-white');
-                    text.classList.remove('text-black')
-                    navbar.classList.remove('bg-[#7180b9]');
-                    navbar.classList.add('bg-[#3423a6]');
-                    page.classList.remove('md:bg-white');
-                    page.classList.add('md:bg-[#171738]');
-                }         
-                else {
-                    text.classList.add('text-black');
-                    text.classList.remove('text-white');
-                    navbar.classList.add('bg-[#7180b9]');
-                    navbar.classList.remove('bg-[#3423a6]');
-                    page.classList.remove('md:wbg-[#171738]');
-                    page.classList.add('md:bg-white');
-                }  
-        })
-            const dot = document.getElementById('cursor-dot');
-
-    window.addEventListener('mousemove', (e) => {
-      dot.style.transform = `translate(${e.clientX - 4}px, ${e.clientY - 4}px)`;
-    });
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    if (window.scrollY > 800) {
+        text.classList.add('text-white');
+        text.classList.remove('text-black')
+        navbar.classList.remove('bg-[#7180b9]');
+        navbar.classList.add('bg-[#3423a6]');
+        page.classList.remove('md:bg-white');
+        page.classList.add('md:bg-[#171738]');
+    }         
+    else {
+        text.classList.add('text-black');
+        text.classList.remove('text-white');
+        navbar.classList.add('bg-[#7180b9]');
+        navbar.classList.remove('bg-[#3423a6]');
+        page.classList.remove('md:wbg-[#171738]');
+        page.classList.add('md:bg-white');
+     }  
+})
